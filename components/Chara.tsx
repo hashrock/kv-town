58b5ba384fe1d686113bd7b3173d6856e5e9d278
hashrock <hashrock@users.noreply.github.com>
2023-06-03 21:38:36 +0900

    improve animation

--- components/Chara.tsx
+++ components/Chara.tsx
@@ -28,10 +28,12 @@ export function Chara({ x, y, username, messages, uid, color }: CharaProps) {
   const [y1, setY1] = useState(y);
   const [x2, setX2] = useState(x);
   const [y2, setY2] = useState(y);
+  const [z, setZ] = useState(0);
+  const [rotation, setRotation] = useState(0);
 
   const [t, setT] = useState(1);
   const [duration, setDuration] = useState(0);
-  const speed = 500;
+  const speed = 400;
 
   useEffect(() => {
     setX1(x);
@@ -43,8 +45,8 @@ export function Chara({ x, y, username, messages, uid, color }: CharaProps) {
 
   useEffect(() => {
     const old = {
-      x: interpolate(t, x1, x2),
-      y: interpolate(t, y1, y2),
+      x: interpolate(easeOutSine(t), x1, x2),
+      y: interpolate(easeOutSine(t), y1, y2),
     };
     setX1(old.x);
     setY1(old.y);
@@ -72,15 +74,18 @@ export function Chara({ x, y, username, messages, uid, color }: CharaProps) {
 
   const animationInterval = useRef(0);
   const [walkTimer, setWalkTimer] = useState(0);
-
+  function easeOutSine(x: number): number {
+    return Math.sin((x * Math.PI) / 2);
+  }
   const animate = (ts: number) => {
-    const x = interpolate(t, x1, x2);
-    const y = interpolate(t, y1, y2);
+    const x = interpolate(easeOutSine(t), x1, x2);
+    const y = interpolate(easeOutSine(t), y1, y2);
 
     if (svgRef && svgRef.current) {
       svgRef.current.style.transform = `translate(${x}px, ${y}px)`;
     }
     const delta = 1 / duration / 100;
+    let isWalkTemp = isWalk;
     if (t < 1 - delta) {
       setT((t) => t + delta);
     } else {
@@ -88,11 +93,24 @@ export function Chara({ x, y, username, messages, uid, color }: CharaProps) {
       setY1(y2);
       setT(0);
       setIsWalk(false);
+      isWalkTemp = false;
+    }
+
+    if (isWalkTemp) {
+      const fadeout = t > 0.7 ? 1 - (t - 0.7) / 0.3 : 1;
+
+      const amp = -Math.abs(Math.cos(ts / 80)) * 17;
+      const rot = Math.floor(Math.sin(ts / 80) * 4);
+      setZ(amp * fadeout);
+      setRotation(rot);
+    } else {
+      setZ(0);
+      setRotation(0);
     }
 
     if (ts - walkTimer > 100 && isWalk) {
       setFrame((frame) => (frame + 1) % 4);
-      setWalkTimer(ts);
+      setWalkTimer((t) => t + 100);
     }
 
     animationInterval.current = requestAnimationFrame(animate);
@@ -136,7 +154,9 @@ export function Chara({ x, y, username, messages, uid, color }: CharaProps) {
         </text>
         <WalkDeno
           x={-50}
-          y={-100}
+          y={0}
+          z={z}
+          rotate={rotation}
           index={frame}
           direction={direction}
           color={color}
