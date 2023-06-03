1e66e64ba9dabca3e43e64a26579b77b3cdc619c
hashrock <hashrock@users.noreply.github.com>
2023-06-03 23:32:58 +0900

    add icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,17 @@
 import { User } from "🛠️/types.ts";
 import { UserNameHorizontal } from "./User.tsx";
-
+import IconHome from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/home.tsx";
 const linkClass = "text-sm text-blue-500 hover:underline";
 
 export function Header(props: { user: User | null }) {
   return (
     <>
       <div class="flex justify-between items-center text-gray-200 mb-2">
-        <a href="/">
-          <h1 class="text-2xl font-bold">KV Town</h1>
+        <a href="/" class="flex gap-2">
+          <IconHome class="w-7 h-7" />
+          <h1 class="text-2xl font-bold">
+            KV Town
+          </h1>
         </a>
         <div class="flex items-end gap-2 justify-between">
           {props.user
